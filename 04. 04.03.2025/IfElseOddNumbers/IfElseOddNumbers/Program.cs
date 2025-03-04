16d0fa7037ebf1de18d9c2c642814b2825e5669c
Ingvar Derešivski <inde999@gmail.com>
2025-03-04 11:14:15 +0200

    IfElseOddNumbers

---
+++ 04. 04.03.2025/IfElseOddNumbers/IfElseOddNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace IfElseOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+            //konsool k[sib numbrit ja siin muudetakse
+            //see int andmetüübiks
+            int number = int.Parse(Console.ReadLine());
+
+            //teha if ja else-ga kontroll, 
+            //kus saame teada, et kas nr
+            //on paaris v]i paaritu
+            //kasuta operaatoreid
+
+            if (number %2 == 0)
+            {
+                Console.WriteLine("Sisestasid paaris arvu: " + number);
+            }
+            else
+            {
+                Console.WriteLine("Sisestasid paaritu arvu: " + number);
+            }
+        }
+    }
+}
