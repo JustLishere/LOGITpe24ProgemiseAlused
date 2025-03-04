44d5e6c4ce8e5859fd6c82984eaf20351c44fadf
Ingvar Derešivski <inde999@gmail.com>
2025-03-04 11:27:10 +0200

    Methodcall

---
+++ 04. 04.03.2025/MethodCall/MethodCall/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //kutsuda esile meetod Jalgratas
+
+            Jalgratas();
+        }
+
+        static void Jalgratas()
+        {
+            Console.WriteLine("Nüüd oled jalgratta meetodis");
+        }
+    }
+}
